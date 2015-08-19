35d17a2dee27aeac5aa48c97a8efe4429803aed7
mingang.he <dustgle@gmail.com>
2015-08-19 11:07:14 +0800

    update

--- src/tcfsd/tcfsd.go
+++ src/tcfsd/tcfsd.go
@@ -14,6 +14,7 @@ import (
 
 func handleConn(conn net.Conn) {
 	var err error
+	buf := make([]byte, 4096*1024) // 4MB
 	reGetattr := regexp.MustCompile("^getattr(.*)")
 	reReaddir := regexp.MustCompile("^readdir(.*)")
 	reOpen := regexp.MustCompile("^open(.*)")
@@ -28,42 +29,39 @@ func handleConn(conn net.Conn) {
 	reUtime := regexp.MustCompile("^utime(.*)")
 	rootdir := "/home/gle/code_repo/cloud_lib/tcfs-go/rootdir"
 	for {
-		buf := make([]byte, 4)
-		_, err = io.ReadFull(conn, buf)
+		_, err = io.ReadFull(conn, buf[:4])
 		if err != nil {
 			break
 		}
-		msglen := binary.BigEndian.Uint32(buf)
-		buf = make([]byte, msglen)
-		_, err = io.ReadFull(conn, buf)
+		msglen := binary.BigEndian.Uint32(buf[:4])
+		_, err = io.ReadFull(conn, buf[:msglen])
 		// println(string(buf))
-		if matched := reGetattr.FindStringSubmatch(string(buf)); len(matched) > 1 {
+		if matched := reGetattr.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
 			fixpath := rootdir + matched[1]
 			var stat syscall.Stat_t
 			err = syscall.Lstat(fixpath, &stat)
 			if err != nil {
-				buf = make([]byte, 4+4)
 				binary.BigEndian.PutUint32(buf[0:4], 4)
 				var ret int32 = -2
 				binary.BigEndian.PutUint32(buf[4:8], uint32(ret))
-				conn.Write(buf)
+				conn.Write(buf[:8])
 				continue
 			}
-			buf = make([]byte, 4+11*4)
 			binary.BigEndian.PutUint32(buf[0:4], 11*4)
-			binary.BigEndian.PutUint32(buf[4:8], uint32(stat.Dev))
-			binary.BigEndian.PutUint32(buf[8:12], uint32(stat.Ino))
-			binary.BigEndian.PutUint32(buf[12:16], stat.Mode)
-			binary.BigEndian.PutUint32(buf[16:20], uint32(stat.Nlink))
-			binary.BigEndian.PutUint32(buf[20:24], stat.Uid)
-			binary.BigEndian.PutUint32(buf[24:28], stat.Gid)
-			binary.BigEndian.PutUint32(buf[28:32], uint32(stat.Size))
-			binary.BigEndian.PutUint32(buf[32:36], uint32(stat.Atim.Sec))
-			binary.BigEndian.PutUint32(buf[36:40], uint32(stat.Mtim.Sec))
-			binary.BigEndian.PutUint32(buf[40:44], uint32(stat.Ctim.Sec))
-			conn.Write(buf)
+			binary.BigEndian.PutUint32(buf[4:8], 0)
+			binary.BigEndian.PutUint32(buf[8:12], uint32(stat.Dev))
+			binary.BigEndian.PutUint32(buf[12:16], uint32(stat.Ino))
+			binary.BigEndian.PutUint32(buf[16:20], stat.Mode)
+			binary.BigEndian.PutUint32(buf[20:24], uint32(stat.Nlink))
+			binary.BigEndian.PutUint32(buf[24:28], stat.Uid)
+			binary.BigEndian.PutUint32(buf[28:32], stat.Gid)
+			binary.BigEndian.PutUint32(buf[32:36], uint32(stat.Size))
+			binary.BigEndian.PutUint32(buf[36:40], uint32(stat.Atim.Sec))
+			binary.BigEndian.PutUint32(buf[40:44], uint32(stat.Mtim.Sec))
+			binary.BigEndian.PutUint32(buf[44:48], uint32(stat.Ctim.Sec))
+			conn.Write(buf[:48])
 
-		} else if matched := reReaddir.FindStringSubmatch(string(buf)); len(matched) > 1 {
+		} else if matched := reReaddir.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
 			fixpath := rootdir + matched[1]
 			fileList := []string{}
 			err = filepath.Walk(fixpath, func(path string, f os.FileInfo, err error) error {
@@ -73,16 +71,16 @@ func handleConn(conn net.Conn) {
 			for _, file := range fileList {
 				fmt.Println(file)
 			}
-		} else if matched := reOpen.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reRead.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reWrite.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reTruncate.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reRelease.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reMkdir.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reRmdir.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reUnlink.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reCreate.FindStringSubmatch(string(buf)); len(matched) > 1 {
-		} else if matched := reUtime.FindStringSubmatch(string(buf)); len(matched) > 1 {
+		} else if matched := reOpen.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reRead.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reWrite.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reTruncate.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reRelease.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reMkdir.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reRmdir.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reUnlink.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reCreate.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
+		} else if matched := reUtime.FindStringSubmatch(string(buf[:msglen])); len(matched) > 1 {
 		} else {
 		}
 	}
